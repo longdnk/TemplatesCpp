40fc19f56c47a2c7dd459016096ac36f3adaa091
longdnk <kimlong101020@gmail.com>
2023-07-11 11:30:49 +0700

    update

--- wtf.cpp
+++ wtf.cpp
@@ -2,49 +2,88 @@
 
 using namespace std;
 
+struct Data {
+    int value;
+    int priority;
+};
+
 struct Node {
-    int key;
-    Node *prev;
+    Data key;
     Node *next;
 };
 
-bool deleteNodeAtK(Node *&head, int k) {
-    if (k > getListSize(head) || k <= 0 || isEmpty(head)) {
-        return false;
+struct Queue {
+    Node *head;
+    Node *tail;
+    Queue() : head(NULL), tail(NULL) { }
+};
+
+Node *createNode(Data x) {
+    Node *p = new Node;
+    p->key = x;
+    p->next = NULL;
+    return p;
+}
+
+bool isEmpty(Queue *q) {
+    return q->head == NULL;
+}
+
+void enQueue(Queue *&q, Data k) {
+    Node *p = createNode(k);
+    if (isEmpty(q)) {
+        q->head = q->tail = p;
     }
-    Node *p = head;
-    while (p != NULL && k > 1) {
-        --k;
-        p = p->next;
+    else {
+        Node *tmp = q->head;
+        Node *prev = NULL;
+        while (tmp != NULL && tmp->key.priority > k.priority) {
+            prev = tmp;
+            tmp = tmp->next;
+        }
+        if (tmp != NULL) {
+            if (prev == NULL) {
+                p->next = q->head;
+                q->head = p;
+            }
+            else {
+                p->next = prev->next;
+                prev->next = p;
+            }
+        }
+        if (tmp == NULL) {
+            q->tail->next = p;
+            q->tail = p;
+        }
     }
-    if (p != NULL) {
-        if (p->prev != NULL) {
-            p->prev->next = p->next;
-        }
-        else {
-            head = p->next;
-        }
-        if (p->next != NULL) {
-            p->next->prev = p->prev;
-        }
+}
+
+bool deQueue(Queue *&q) {
+    if (!isEmpty(q)) {
+        Node *p = q->head;
+        q->head = p->next;
+        p->next = NULL;
         delete p;
+        if (isEmpty(q)) {
+            q->tail = NULL;
+        }
         return true;
     }
-}
-
-int getListSize(Node *head) {
-    if (isEmpty(head)) {
-        return 0;
-    }
-    return getListSize(head->next) + 1;
-}
-
-bool isEmpty(Node *head) {
-    return head == NULL;
+    return false;
 }
 
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    Queue *q = new Queue;
+    enQueue(q, { 1, 3 });
+    enQueue(q, { 2, 5 });
+    enQueue(q, { 10, 1 });
+    enQueue(q, { 11, 2 });
+    while (!isEmpty(q)) {
+        auto [value, priority] = q->head->key;
+        cout << value << ' ' << priority << '\n';
+        q->head = q->head->next;
+    }
 }
