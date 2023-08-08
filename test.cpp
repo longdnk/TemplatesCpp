42a5d965dd1ab3f758f7b17041f6422647e904cc
longdnk <kimlong101020@gmail.com>
2023-08-08 17:05:03 +0700

    Update all code

--- test.cpp
+++ test.cpp
@@ -2,97 +2,53 @@
 
 using namespace std;
 
-// để tao ra một cây nhị phân tìm kiếm từ cây nhị phân đề cho
-// ta duyệt cây theo một thứ tự nhất định (node - left - right)
-// với mỗi lần duyệt ta tiến hành gọi hàm chèn kiểm tra nút có hợp lệ hay không 
-// sau đó chèn vào cây mới 
-
-// A
 struct Node {
     int key;
-    Node *left;
-    Node *right;
+    Node *next;
 };
 
-void createBST(Node *&root, Node *tmp) {
-    if (tmp == nullptr) {
-        return;
-    }
-    insertNode(root, tmp);
-    if (root->left != nullptr) {
-        createBST(root, tmp->left);
-    }
-    if (root->right != nullptr) {
-        createBST(root, tmp->right);
-    }
-}
+struct List {
+    Node *head;
+    Node *tail;
+    List() : head(nullptr), tail(nullptr) { }
+};
 
-bool insertNode(Node *&root, Node *p) {
-    if (root == nullptr) {
-        return insert(root, p);
-    }
-    if (root->key == p->key) {
+bool addHead(List *&ls, int k) {
+    Node *p = new Node;
+    if (p == nullptr) {
         return false;
     }
-    else if (root->key > p->key) {
-        return insertNode(root->left, p);
+    p->key = k;
+    p->next = nullptr;
+    if (ls->head == nullptr) {
+        ls->head = ls->tail = p;
     }
     else {
-        return insertNode(root->right, p);
+        p->next = ls->head;
+        ls->head = p;
+        ls->tail->next = p;
     }
-}
-
-bool insert(Node *&root, Node *p) {
-    Node *tmp = new Node;
-    if (tmp == nullptr) {
-        return false;
-    }
-    tmp->key = p->key;
-    tmp->left = tmp->right = nullptr;
-    root = tmp;
     return true;
 }
 
-// B
-Node *createNode(int x) {
-    Node *p = new Node;
-    p->key = x;
-    p->left = p->right = nullptr;
-    return p;
-}
-
-bool validSubTree(Node *root, int kRoot, int kLeft, int kRight) {
-    Node *tmp = createNode(kRoot);
-    tmp->left = createNode(kLeft);
-    tmp->right = createNode(kRight);
-    return isSubTree(root, tmp);
-}
-
-bool isSubTree(Node *root, Node *subRoot) {
-    if (checkSubTree(root, subRoot)) {
-        return true;
+void output(List *ls) {
+    if (ls->head) {
+        Node *p = ls->head;
+        do {
+            cout << p->key << ' ';
+            p = p->next;
+        } while (p != ls->head);
     }
-    if (root == nullptr || subRoot == nullptr) {
-        return false;
-    }
-    return isSubTree(root->left, subRoot) || isSubTree(root->right, subRoot);
-}
-
-bool checkSubTree(Node *root, Node *subRoot) {
-    if (root == nullptr && subRoot == nullptr) {
-        return true;
-    }
-    if (root == nullptr || subRoot == nullptr) {
-        return false;
-    }
-    if (root->key != subRoot->key) {
-        return false;
-    }
-    return checkSubTree(root->left, subRoot->left) && checkSubTree(root->right, subRoot->right);
+    cout << '\n';
 }
 
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    List *ls = new List;
+    addHead(ls, 10);
+    addHead(ls, 20);
+    addHead(ls, 30);
+    output(ls);
 }
--- wtf.cpp
+++ wtf.cpp
@@ -5,10 +5,11 @@ using namespace std;
 struct Node {
 	int key;
 	Node *next;
+	Node *prev;
 
 	Node() = default;
 
-	Node(int x) : key(x), next(nullptr) { }
+	Node(int x) : key(x), next(nullptr), prev(nullptr) { }
 };
 
 struct List {
@@ -18,185 +19,105 @@ struct List {
 	List() : head(nullptr), tail(nullptr) { }
 };
 
-void addHead(List *&ls, int k) {
-	Node *p = new Node(k);
+void displayList(List *&ls) {
+	if (ls->head == nullptr) {
+		return;
+	}
+
+	Node *current = ls->head;
+	do {
+		cout << current->key << " -> ";
+		current = current->next;
+	} while (current != ls->head);
+
+	cout << '\n';
+
+	Node *p = ls->tail;
+	do {
+		cout << p->key << " -> ";
+		p = p->prev;
+	} while (p != ls->tail);
+}
+
+void addToHead(List *&ls, int val) {
+	Node *p = new Node(val);
 	if (ls->head == nullptr) {
 		ls->head = ls->tail = p;
+		p->next = p->prev = p;
 	}
 	else {
 		p->next = ls->head;
+		p->prev = ls->head->prev;
+		ls->head->prev->next = p;
+		ls->head->prev = p;
 		ls->head = p;
 	}
-	ls->tail->next = ls->head;
 }
 
-void addLast(List *&ls, int k) {
-	Node *p = new Node(k);
+void addToTail(List *&ls, int val) {
+	Node *p = new Node(val);
 	if (ls->head == nullptr) {
 		ls->head = ls->tail = p;
+		p->next = p->prev = p;
 	}
 	else {
-		ls->tail->next = p;
+		p->next = ls->head;
+		p->prev = ls->head->prev;
+		ls->head->prev->next = p;
+		ls->head->prev = p;
 		ls->tail = p;
 	}
-	ls->tail->next = ls->head;
 }
 
-Node *searchNode(List *ls, int k) {
+bool addAfter(List *&ls, int prevValue, int val) {
 	if (ls->head != nullptr) {
-		Node *p = ls->head;
-		do {
-			if (p->key == k) {
-				return p;
-			}
-			p = p->next;
-		} while (p != ls->head);
-	}
-	return nullptr;
-}
-
-bool addAfter(List *&ls, int v, int k) {
-	Node *q = searchNode(ls, v);
-	if (q != nullptr) {
-		Node *p = new Node(k);
-		p->next = q->next;
-		q->next = p;
-		if (q == ls->tail) {
-			ls->tail = p;
-		}
-		return true;
-	}
-	return false;
-}
-
-Node *searchElement(List *ls, Node *&prev, int k) {
-	Node *p = ls->head;
-	do {
-		if (p->key == k) {
-			return p;
-		}
-		prev = p;
-		p = p->next;
-	} while (p != ls->head);
-	return nullptr;
-}
-
-bool addBefore(List *&ls, int v, int k) {
-	Node *prev = nullptr;
-	Node *q = searchElement(ls, prev, v);
-	if (q != nullptr) {
-		if (prev == nullptr) {
-			addHead(ls, k);
-		}
-		else {
-			Node *p = new Node(k);
-			p->next = prev->next;
-			prev->next = p;
-		}
-		return true;
-	}
-	return false;
-}
-
-bool deleteHead(List *&ls) {
-	if (ls->head != nullptr) {
-		Node *p = ls->head;
-		if (ls->head == ls->tail) {
-			ls->head = ls->tail = nullptr;
-		}
-		else {
-			ls->head = p->next;
-			ls->tail->next = ls->head;
-		}
-		delete p;
-		return true;
-	}
-	return false;
-}
-
-bool deleteLast(List *&ls) {
-	if (ls->head != nullptr) {
-		Node *last = ls->head;
-		Node *prev = nullptr;
-		while (last != ls->tail) {
-			prev = last;
-			last = last->next;
-		}
-		if (prev == nullptr) {
-			return deleteHead(ls);
-		}
-		prev->next = last->next;
-		ls->tail = prev;
-		delete last;
-		return true;
-	}
-	return false;
-}
-
-bool deleteNode(List *&ls, int k) {
-	if (ls->head != nullptr) {
-		Node *prev = nullptr;
-		Node *p = searchElement(ls, prev, k);
+		Node *prevNode = ls->head;
 		bool flag = false;
-		if (p != nullptr) {
+		while (prevNode != nullptr && prevNode->key != prevValue) {
+			prevNode = prevNode->next;
+		}
+		if (prevNode != nullptr) {
+			Node *p = new Node(val);
+			p->next = prevNode->next;
+			p->prev = prevNode;
+
+			prevNode->next = p;
+			p->next->prev = p;
+
+			if (prevNode == ls->tail) {
+				ls->tail = p;
+			}
 			flag = true;
-			if (prev == nullptr) {
-				return deleteHead(ls);
-			}
-			prev->next = p->next;
-			p->next = nullptr;
-			if (p == ls->tail) {
-				ls->tail = prev;
-			}
-			delete p;
 		}
 		return flag;
 	}
 	return false;
 }
 
-int getListSize(List *ls) {
-	Node *p = ls->head;
-	int size = 0;
-	do {
-		++size;
-		p = p->next;
-	} while (p != ls->head);
-	return size;
-}
-
-bool deleteNodeAtK(List *&ls, int k) {
-	if (k <= 0 || k > getListSize(ls) || ls->head == nullptr) {
-		return false;
+bool addBefore(List *&ls, int v, int k) {
+	if (ls->head != nullptr) {
+		Node *prevNode = ls->head;
+		bool flag = false;
+		while (prevNode != nullptr && prevNode->key != v) {
+			prevNode = prevNode->next;
+		}
+		if (prevNode != nullptr) {
+			Node *prev = prevNode->prev;
+			if (prev == ls->tail) {
+				addToHead(ls, k);
+			}
+			else {
+				Node *p = new Node(k);
+				p->next = prevNode->next;
+				p->prev = prevNode;
+				prevNode->next = p;
+				p->next->prev = p;
+			}
+			flag = true;
+		}
+		return flag;
 	}
-	Node *p = ls->head;
-	Node *prev;
-	do {
-		--k;
-		prev = p;
-		p = p->next;
-	} while (p != ls->head && k > 1);
-	if (k == 0) {
-		return deleteHead(ls);
-	}
-	if (p == ls->tail) {
-		ls->tail = prev;
-	}
-	prev->next = p->next;
-	p->next = nullptr;
-	delete p;
-	return true;
-}
-
-void output(List *ls) {
-	if (ls->head) {
-		Node *p = ls->head;
-		do {
-			cout << p->key << ' ';
-			p = p->next;
-		} while (p != ls->head);
-	}
-	cout << '\n';
+	return false;
 }
 
 int32_t main() {
@@ -204,17 +125,16 @@ int32_t main() {
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 	List *ls = new List;
-//	addHead(ls, 10);
-//	addHead(ls, 20);
-//	addHead(ls, 30);
-	addLast(ls, 10);
-	addLast(ls, 20);
-	addLast(ls, 30);
-	addAfter(ls, 30, 50);
-	addBefore(ls, 50, 40);
-//	deleteHead(ls);
-//	deleteLast(ls);
-//	deleteNode(ls, 10);
-//	deleteNodeAtK(ls, 1);
-	output(ls);
+//	addToHead(ls, 10);
+//	addToHead(ls, 20);
+//	addToHead(ls, 30);
+//	addToHead(ls, 40);
+//	displayList(ls);
+	addToTail(ls, 10);
+	addToTail(ls, 20);
+	addToTail(ls, 30);
+	addToTail(ls, 40);
+	addAfter(ls, 40, 60);
+	addBefore(ls, 40, 50);
+	displayList(ls);
 }
